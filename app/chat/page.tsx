4eb155a307cb40815d3cae39707845cb3e9a3683
muhammadBilal1029 <167223444+muhammadBilal1029@users.noreply.github.com>
2026-06-08 17:37:10 +0500

    Update page.tsx

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -311,13 +311,28 @@ setCallConnected(false);
         }
     };
     const createPeer = () => {
-        const peer = new RTCPeerConnection({
-            iceServers: [
-                {
-                    urls: 'stun:stun.l.google.com:19302',
-                },
-            ],
-        });
+       const peer = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject',
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject',
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        username: 'openrelayproject',
+        credential: 'openrelayproject',
+      },
+    ],
+  });
 
         peer.ontrack = (event) => {
             console.log('Remote stream received');
